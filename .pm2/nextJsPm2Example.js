1374da582c8c8d98a89b89f821015b7a23d8380b
Kirill Zhirnov <ekvixgroup@gmail.com>
2023-08-04 19:20:35 +0200

    upd

--- .pm2/nextJsPm2Example.js
+++ .pm2/nextJsPm2Example.js
@@ -6,16 +6,13 @@ module.exports = {
 		{
 			name: 'nextjs-app',
 			cwd: rootPath,
-			//specify path to Nextjs execution file:
 			script: 'node_modules/next/dist/bin/next',
-			//specify argument which will be passed to the next file
 			args: 'start',
 			instances: 0,
 			exec_mode: 'cluster',
 			watch: false,
 			merge_logs: true,
-			//pass env-vars to workers
-			env:  {
+			env: {
 				NODE_ENV: 'production'
 			}
 		}
